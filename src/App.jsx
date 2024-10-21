791089c339378c6b21ec00756577123364fe537e
Pechu14 <jesus.gil.santos1992@gmail.com>
2024-10-21 17:05:03 +0200

    ejercicio terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+
 
 function App() {
  //Aquí tu código
+const [name, setName ] = useState('Sofía')
+
+
+ const changeName = (newName) => {
+     setName (newName)
+ };
+
+ return (
+    <div>
+    <h2>Teacher Name: {name}</h2>
+    <ul>
+        <li><button onClick = {() => changeName('Data')}>Data</button></li>
+        <li><button onClick = {() => changeName('Reyes')}>Reyes</button></li>
+        <li><button onClick = {() => changeName('Yolanda')}>Yolanda</button></li>
+    </ul>
+    </div>
+ );
 }
 
-export default App
+export default App;
+
+
+
